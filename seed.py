90efc2ad0a44970ff757494238f23f9afffde9b3
rahman1337 <78167584+rahman1337@users.noreply.github.com>
2025-11-08 17:52:34 +0800

    Update seed.py

--- seed.py
+++ seed.py
@@ -5,6 +5,7 @@ from mnemonic import Mnemonic
 from eth_account import Account
 from concurrent.futures import ThreadPoolExecutor
 import time
+Account.enable_unaudited_hdwallet_features()
 
 # ---------------- SETTINGS ----------------
 RPC_URL = "https://ethereum.publicnode.com"
